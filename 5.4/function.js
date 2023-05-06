e0e2c4418971e872ee8aee563cc474b09d230f23
MRTSsss <tsogoobatkhuu845@gmail.com>
2023-05-06 19:31:55 +0800

    function

---
+++ 5.4/function.js
@@ -0,0 +1,26 @@
+/*function add_numbers(a,b){
+    let=sum=0;
+    sum=a+b;
+    console.log(sum);
+}
+function tegsh_too_oloh(a,b) {
+    if(a%2===0 && b%2===0){
+        console.log(a,b);
+    }else if (a%2===0) {
+        console.log(a);
+    }else if(b%2===0){
+        console.log(b);
+    }else{
+        console.log("tegsh too alga");
+    }
+} */
+/*for (let index = 1; index <= 100; index++) {
+    console.log(index); 
+}
+print(index);*/
+/*function print_100(){
+    let x=prompt("too oruul")
+for (let index = 1; index <= 100; index++) {
+    console.log(index); 
+}
+}  print_100() */ 
